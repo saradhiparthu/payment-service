506959eb6a48c88df4a314fa9edcc23d682f41d2
saradhiparthu <saradhiparthu@gmail.com>
2024-12-29 15:55:45 -0500

    Initial commit

---
+++ src/main/java/com/microservices/controller/PaymentController.java
@@ -0,0 +1,14 @@
+package com.microservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("payment")
+public class PaymentController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello Pulivarthi it's Payment api %s!", name);
+    }
+}
